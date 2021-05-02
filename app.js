15a426176834b04d2bfc3d2e67c68cfcb122f690
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-02 14:45:53 +0200

    Add: middleware on  api route transfer

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const apiRouter = require();
+const apiRouterTransfers = require('./routes/api/v1/transfers');
 
 const app = express();
 
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1/transfers', apiRouterTransfers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
