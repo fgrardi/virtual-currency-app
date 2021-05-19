9e3146bafa7d2e4c3d8a0a87f468fa543d95d403
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-19 18:02:10 +0200

    Feat: change objects of user to include username, password and email

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,13 @@ const passport = require('../passport/passport');
 const signup = async (req, res, next) => {
     let username = req.body.username;
     let password = req.body.password;
+    let email = req.body.email;
 
-    const user = new User({username: username});
+    const user = new User({
+        username: username,
+        password: password,
+        email: email
+    });
     await user.setPassword(password);
     await user.save().then(result => {
         res.json({
