8b670bab329fb609985914bc701d71e65e8c854d
FlorVk <florvankuyk@hotmail.com>
2021-05-21 12:54:05 +0200

    Add: basic html files

---
+++ routes/login.js
@@ -0,0 +1,31 @@
+const base_url = "https://todo-nodejs-goodbytes.herokuapp.com";
+
+var btnLogin = document.querySelector(".login button").addEventListener("click", (e) => {
+    let email = document.querySelector('#email').value;
+    let username = document.querySelector('#username').value;
+    let password = document.querySelector('#password').value;
+
+    fetch(base_url + '/users/login', {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "email": email,
+            "username": username,
+            "password": password
+        })
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if (json.status === "success") {
+            let token = json.data.token;
+            localStorage.setItem("token", token);
+            window.location.href = "index.html";
+        } else {
+            let feedback = document.querySelector(".alert");
+            feedback.textContent = "Login failed buddy.";
+            feedback.classList.remove('hidden');
+        }
+    })
+});
