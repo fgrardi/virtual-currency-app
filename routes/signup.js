f01c20e480c2e4104be32e82e26589228f97ada3
FlorVk <florvankuyk@hotmail.com>
2021-05-22 15:33:57 +0200

    Add: Signup can only with a thomasmore email

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,21 @@
 var btnSignup = document.querySelector(".signup button").addEventListener("click", (e) => {
 let email = document.querySelector('#email').value;
 let password = document.querySelector('#password').value;
+function validateEmail(){
+    let allowedDomain = ['student.thomasmore.be'];
+    let emailValue = document.getElementById("email").value;
+    let splitArray = emailValue.split('@');
+    if (allowedDomain.indexOf(splitArray[1]) >=0)
+  {
+      console.log(emailValue);
+    return (true)
+  }
+    let feedback = document.querySelector(".alert__email");
+    feedback.textContent = "Invalid email";
+    feedback.classList.remove('hidden');
+    return (false)
+}
+validateEmail();
 
     fetch('http://localhost:3000/users/signup', {
         method: "post",
@@ -13,11 +28,13 @@ let password = document.querySelector('#password').value;
         })
     }).then(response => {
         return response.json();
+        
     }).then(json => {
         if (json.status === "success") {
-            let feedback = document.querySelector(".alert");
+            let feedback = document.querySelector(".alert__fail");
             feedback.textContent = "Sign up complete!";
             feedback.classList.remove('hidden');
         }
     })
 });
+
