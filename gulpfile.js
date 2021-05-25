f8027c6c7aafb2e8cf9e3d530a28fab3c507ad3f
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-25 19:49:06 +0200

    Feat: add sass2css function in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
-const {src, dest} = require("gulp");
+const {watch, src, dest} = require('gulp');
+const sass = require('gulp-sass');
 
-function tryout(done){
-    console.log("It works!");
+// function tryout(done){
+//     console.log("It works!");
+//     done();
+// }
+
+function sass2css(done){
+	return src('./src/**/*.scss').pipe(sass().on("error", sass.logError)).pipe(gulp.dest('./public/stylesheets'));
+	done();
+}
+
+exports.default = function(done) {
+	watch('./src/**/*.scss', sass2css);
     done();
 }
 
-exports.tryout = tryout;
+exports.sass2css = sass2css;
+//exports.tryout = tryout;
