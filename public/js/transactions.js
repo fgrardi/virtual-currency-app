b676fcae0e51a4a0cd0d7c2495c3e9b98255d182
FlorVk <florvankuyk@hotmail.com>
2021-05-28 18:17:50 +0200

    Feat: transactions

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -13,7 +13,7 @@ const searchUser = async searchText => {
     const res = await resAPI.json();
     let matches = res.filter(user => {
         const regex = new RegExp(`^${searchText}`, 'gi');
-        return user.username.match(regex) || user.name.match(regex);
+        return user.firstname.match(regex) || user.lastname.match(regex);
     });
 
     if(searchText.lenght === 0){
@@ -28,7 +28,7 @@ const outputHtml = matches => {
     if(matches.length > 0){
         const html = matches.map(match => `
             <div>
-                <p>${match.username} </p>
+                <p>${match.firstname} ${match.lastname} </p>
             </div>
         `).join('');
 
@@ -37,4 +37,38 @@ const outputHtml = matches => {
 }
 
 search.addEventListener('input', ()=> searchUser(search.value));
+
+var btnSignup = document.querySelector(".transaction button").addEventListener("click", (e) => {
+    let firstname = document.querySelector('#username').value;
+    let coins = document.querySelector('#coins').value;
+    let reason = document.querySelector('#reason').value;
+    let message = document.querySelector('#message').value;
+    
+        fetch('/users/signup', {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "firstname": firstname,
+                "coins": coins,
+                "reason": reason,
+                "message": message
+            })
+        }).then(response => {
+            return response.json();
+            
+        }).then(json => {
+            if (json.status === "success") {
+                let feedback = document.querySelector(".alert__fail");
+                feedback.textContent = "Transfer succesful!";
+                feedback.classList.remove('hidden');
+            }
+            else{
+                let feedback = document.querySelector(".alert__fail");
+                feedback.textContent = "Something went wrong!";
+                feedback.classList.remove('hidden');
+            }
+        })
+    });
 
