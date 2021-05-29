9482a6e959017350684c32e20d51f24593809585
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-29 17:49:16 +0200

    Add firstname, lastname and username to frontend in signup + include gmail as valid

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,12 @@
 var btnSignup = document.querySelector(".signup button").addEventListener("click", (e) => {
 let email = document.querySelector('#email').value;
 let password = document.querySelector('#password').value;
+let firstname = document.querySelector('#firstname').value;
+let lastname = document.querySelector('#lastname').value;
+let username = document.querySelector('#username').value;
+
 function validateEmail(){
-    let allowedDomain = ['student.thomasmore.be'];
+    let allowedDomain = ['student.thomasmore.be', 'gmail.com'];
     let emailValue = document.getElementById("email").value;
     let splitArray = emailValue.split('@');
     if (allowedDomain.indexOf(splitArray[1]) >=0)
@@ -23,6 +27,9 @@ validateEmail();
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
+            "firstname": firstname,
+            "lastname": lastname,
+            "username": username,
             "email": email,
             "password": password
         })
