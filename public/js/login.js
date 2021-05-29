ec4e747f6f17a6884f16fc07b299c8fb630075c6
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-29 14:22:27 +0200

    Include token into page load to redirect to login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,29 @@
-var btnLogin = document.querySelector(".login button").addEventListener("click", (e) => {
-let email = document.querySelector('#email').value;
-let password = document.querySelector('#password').value;
+var btnLogin = document.querySelector(".login__btn");
 
-    fetch('/users/login', {
-        method: "post",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "email": email,
-            "password": password
+btnLogin.addEventListener("click", (e) => {
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+        fetch('/users/login', {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "email": email,
+                "password": password
+            })
+        }).then(response => {
+            return response.json();
+        }).then(json => {
+            if (json.status === "success") {
+                let token = json.data.token;
+                localStorage.setItem("token", token);
+                window.location.href = "index.html";
+            } else {
+                let feedback = document.querySelector(".alert");
+                feedback.textContent = "Login failed.";
+                feedback.classList.remove('hidden');
+            }
         })
-    }).then(response => {
-        return response.json();
-    }).then(json => {
-        if (json.status === "success") {
-            let token = json.data.token;
-            localStorage.setItem("token", token);
-            window.location.href = "index.html";
-        } else {
-            let feedback = document.querySelector(".alert");
-            feedback.textContent = "Login failed.";
-            feedback.classList.remove('hidden');
-        }
-    })
 });
