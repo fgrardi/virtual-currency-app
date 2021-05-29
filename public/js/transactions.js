73c00104f52fe2d3b26474434459efa13e7d4e00
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-29 18:30:39 +0200

    Insert token into transaction + adjust fetch api to transfers

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -3,50 +3,51 @@ const matchList = document.getElementById('userList');
 
 // autocomplete username
 
-const searchUser = async searchText => {
-    const resAPI = await fetch('/users', { //getdata weggehaald => /users: ik maak dit endpoint (fien)
-        method: "get",
-                headers: {
-                    'Content-Type': 'application/json'
-    }})
+// const searchUser = async searchText => {
+//     const resAPI = await fetch('/users', { //getdata weggehaald => /users: ik maak dit endpoint (fien)
+//         method: "get",
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//     }})
    
-    const res = await resAPI.json();
-    let matches = res.filter(user => {
-        const regex = new RegExp(`^${searchText}`, 'gi');
-        return user.firstname.match(regex) || user.lastname.match(regex);
-    });
+//     const res = await resAPI.json();
+//     let matches = res.filter(user => {
+//         const regex = new RegExp(`^${searchText}`, 'gi');
+//         return user.firstname.match(regex) || user.lastname.match(regex);
+//     });
 
-    if(searchText.length === 0){
-        matches = [];
-        matchList.innerHTML = '';
-    }
+//     if(searchText.length === 0){
+//         matches = [];
+//         matchList.innerHTML = '';
+//     }
 
-    outputHtml(matches);
-};
+//     outputHtml(matches);
+// };
 
-const outputHtml = matches => {
-    if(matches.length > 0){
-        const html = matches.map(match => `
-            <div>
-                <p>${match.firstname} ${match.lastname} </p>
-            </div>
-        `).join('');
+// const outputHtml = matches => {
+//     if(matches.length > 0){
+//         const html = matches.map(match => `
+//             <div>
+//                 <p>${match.firstname} ${match.lastname} </p>
+//             </div>
+//         `).join('');
 
-        match.innerHTML = html
-    }
-}
-search.addEventListener('input', ()=> searchUser(search.value));
+//         match.innerHTML = html
+//     }
+// }
+// search.addEventListener('input', ()=> searchUser(search.value));
 
 var btnSignup = document.querySelector(".transaction button");
 
 btnSignup.addEventListener("click", (e) => {
+    console.log("click")
     let username = document.querySelector('#username').value;
     let amount = document.querySelector('#coins').value;
     let reason = document.querySelector('#reason').value;
     let message = document.querySelector('#message').value;
     let token = localStorage.getItem('token');
     
-    fetch('/users/signup', {
+    fetch('/api/v1/transfers', {
         method: "post",
         headers: {
             'Content-Type': 'application/json',
