16f28dc227e9cfb33d1743689f290c8e6b9c0a50
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-29 16:15:32 +0200

    Fix: wrong auth structure to send to Google

--- mailer/nodemailer.config.js
+++ mailer/nodemailer.config.js
@@ -3,27 +3,26 @@ const config = require('config');
 
 const user = config.get('nodemailer.user');
 const password = config.get('nodemailer.password');
-
 const transport = nodemailer.createTransport({
   service: "Gmail",
   auth: {
     user: user,
-    password: password,
+    pass: password,
   },
 });
 
-const sendConfirmationEmail = (name, email, confirmationCode, host) => {
+const sendConfirmationEmail = (name, firstname, lastname, email, confirmationCode, host) => {
     console.log("Check");
     transport.sendMail({
       from: user,
       to: email,
       subject: "Please confirm your account",
       html: `<h1>Email Confirmation</h1>
-          <h2>Hello ${name}</h2>
+          <h2>Hello ${firstname} ${lastname} (${name})</h2>
           <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
           <a href=http://${host}/users/confirm?code=${confirmationCode}> Click here</a>
           </div>`,
-    }).catch(err => console.log(err));
+    }).catch(err => console.log("Error during send email:", err));
 }
 
 module.exports.sendConfirmationEmail = sendConfirmationEmail;
