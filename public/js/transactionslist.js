261252755e6761acb06ec6d068b4042dc7096e0e
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-29 13:57:46 +0200

    Bugfix: transaction loading + token authorization (hard coded test)

--- public/js/transactionslist.js
+++ public/js/transactionslist.js
@@ -1,24 +1,32 @@
 window.addEventListener("load", function() {
+    console.log("load");
 
         fetch('/api/v1/transfers', {
             method: "get",
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MjIyODg0MjIsImV4cCI6MTYyMjM3NDgyMn0.4OzuQO3hIvqXQfbaViN9rh9AyUUmtksx1NiZ-V7ZlJE'
             }
         }).then(response => {
             return response.json();
         }).then(json => {
+            console.log(json, json.status, json.data);
             if (json.status === "success") {
-                json.data.forEach(element => {
-                    let transactions = `
+                let transactions = json.data.transactions;
+                //console.log(transactions);
+                let transactionHtml;
+
+                transactions.forEach(element => {
+                    console.log(element);
+                    transactionHtml = transactionHtml + `
                     <ul class="transactions--list">
-                    <li class="transaction transactions--name">${data.username}</li>
-                    <li class="transaction transactions--amount">${data.coins}</li>
-                    <li class="transaction transactions--date">${data.date}</li>
+                    <li class="transaction transactions--name">${element.recipient}</li>
+                    <li class="transaction transactions--amount">${element.amount}</li>
+                    <li class="transaction transactions--date">${element.reason}</li>
                     </ul>
                     `
-                    this.document.getElementById('list').innerHTML = transactions;
                 });
+                this.document.getElementById('list').innerHTML = transactionHtml;
             } 
         })
     });
