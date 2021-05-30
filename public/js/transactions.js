388c8d44a8c899738de6c4b19c8273994819e523
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-30 12:41:53 +0200

    Add autocompletion for recipient field

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -1,41 +1,18 @@
-const search = document.getElementById('recipient');
-const matchList = document.getElementById('userList');
-
-// autocomplete username
-
-// const searchUser = async searchText => {
-//     const resAPI = await fetch('/users', { //getdata weggehaald => /users: ik maak dit endpoint (fien)
-//         method: "get",
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//     }})
-   
-//     const res = await resAPI.json();
-//     let matches = res.filter(user => {
-//         const regex = new RegExp(`^${searchText}`, 'gi');
-//         return user.firstname.match(regex) || user.lastname.match(regex);
-//     });
-
-//     if(searchText.length === 0){
-//         matches = [];
-//         matchList.innerHTML = '';
-//     }
-
-//     outputHtml(matches);
-// };
-
-// const outputHtml = matches => {
-//     if(matches.length > 0){
-//         const html = matches.map(match => `
-//             <div>
-//                 <p>${match.firstname} ${match.lastname} </p>
-//             </div>
-//         `).join('');
-
-//         match.innerHTML = html
-//     }
-// }
-// search.addEventListener('input', ()=> searchUser(search.value));
+fetch('/users', {
+    method: "get",
+    headers: { 'Content-Type': 'application/json' }
+})
+.then(response => {
+    return response.json();
+}).then(json => {
+    let users = json.users;
+    let userOptionsHtml = "";
+    let optionsUsers = document.getElementById("user_list");
+    users.forEach(user => {
+         userOptionsHtml += `<option>${user.username}</option>\n`
+    });
+    optionsUsers.innerHTML = userOptionsHtml;
+});
 
 var btnTransaction = document.querySelector(".transaction button");
 
