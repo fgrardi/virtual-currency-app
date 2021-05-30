4af6dff2937f2fd05de06e418ef1d51b5b5e7554
Fien Gérardi <fien.gerardi@gmail.com>
2021-05-30 15:25:41 +0200

    Change layout navigation bar

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -17,13 +17,24 @@ fetch('/users', {
 var btnTransaction = document.querySelector(".transaction button");
 
 btnTransaction.addEventListener("click", (e) => {
-    console.log("click")
+    let alertSuccess = document.querySelector(".alert--success");
+    let alertFail = document.querySelector(".alert--fail");
+
     let recipient = document.querySelector('#recipient').value;
     let amount = document.querySelector('#coins').value;
     let reason = document.querySelector('#reason').value;
     let message = document.querySelector('#message').value;
     let token = sessionStorage.getItem('token');
     let username = sessionStorage.getItem('username');
+
+    console.log("[" + amount + "]");
+    console.log("[" + reason + "]");
+
+    if(amount === "" || recipient === "" || reason === "What is the reason") {
+        alertFail.classList.remove('hidden');
+        alertSuccess.classList.add('hidden');
+        return
+    }
     
     fetch('/api/v1/transfers', {
         method: "post",
@@ -42,16 +53,16 @@ btnTransaction.addEventListener("click", (e) => {
         return response.json();
         
     }).then(json => {
-        let alertSuccess = document.querySelector(".alert--success");
-        let alertFail = document.querySelector(".alert--fail");
 
         if (json.status === "success") {
             alertSuccess.innerHTML = "Transfer succesful!";
             alertSuccess.classList.remove('hidden');
+            alertFail.classList.add('hidden');
         }
         else{
             alertFail.innerHTML = "Something went wrong!";
             alertFail.classList.remove('hidden');
+            alertSuccess.classList.add('hidden');
         }
     })
 });
